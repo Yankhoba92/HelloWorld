ff0363802a7e2b7a8d70396917e9a17ec517c20a
Yankhoba <bagayoko.yan@gmail.com>
2024-04-02 10:19:20 +0200

    Activity indicator

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { useState } from "react";
 import {
-  Text,
   View,
-  Image,
-  ScrollView,
-  Pressable,
-  Button,
-  Modal,
-  StatusBar,
+  ActivityIndicator
 } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <StatusBar backgroundColor="lightgreen" barStyle="light-content"/>
+      <ActivityIndicator/>
+      <ActivityIndicator size="large" color="midnightblue"/>
+      <ActivityIndicator size="large" color="midnightblue" animating={false}/>
     </View>
   );
 }
